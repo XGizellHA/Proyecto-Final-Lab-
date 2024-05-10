3da409ec32dd39b4f09c3f6b2e32474e1798a49d
XGizellHA <160786120+XGizellHA@users.noreply.github.com>
2024-05-09 22:55:19 -0600

    Add files via upload

---
+++ CommonValues.h
@@ -0,0 +1,8 @@
+#ifndef COMMONVALS
+#define COMMONVALS
+#include "stb_image.h"
+
+//Cantidad de luces, si se declaran las sobreescribe las primeras 3 o 4
+const int MAX_POINT_LIGHTS = 3;
+const int MAX_SPOT_LIGHTS = 4; 
+#endif
---
+++ Light.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <glew.h>
+#include <glm.hpp>
+
+class Light
+{
+public:
+	Light();
+	Light(GLfloat red, GLfloat green, GLfloat blue, 
+			GLfloat aIntensity, GLfloat dIntensity);
+
+	~Light();
+
+//protected se usa cuando una clase -> Superclase
+protected:
+	glm::vec3 color;
+	GLfloat ambientIntensity;
+	GLfloat diffuseIntensity;
+};
+
